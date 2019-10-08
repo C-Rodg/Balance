daf3a8d18881a9fd54d5ccd3191387c813a24d5d
C-Rodg <C-Rodg@users.noreply.github.com>
2019-10-07 18:37:49 -0700

    Removed margin hack from card overlay for safe area method

--- src/screens/AddExpenseScreen.js
+++ src/screens/AddExpenseScreen.js
@@ -7,6 +7,7 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -33,23 +34,62 @@ class AddExpenseScreen extends Component {
     return (
       <Fragment>
         <StatusBar barStyle="light-content" />
-        <SafeAreaView style={styles.appScreen}>
-          <View style={styles.contentWrapper}>
-            <View style={styles.categorySection}>
-              <MaterialCommunityIcon size={72} color={COLORS.white} name="headphones" />
-              <Text>CATEGORY_NAME_CLICKABLE</Text>
-              <Text>Total Budget is $1,250</Text>
-            </View>
-            <View style={styles.calculatorSection}>
-              <View style={styles.currentExpenseSection}>
-                <Text>$00.00</Text>
-              </View>
-              <View>
-                <Text>Calculator stuff</Text>
-              </View>
-            </View>
+        <SafeAreaView />
+        <View style={styles.contentWrapper}>
+          <View style={styles.categorySection}>
+            <MaterialCommunityIcon
+              size={72}
+              color={COLORS.white}
+              name="headphones"
+            />
+            <Text>CATEGORY_NAME_CLICKABLE</Text>
+            <Text>Total Budget is $1,250</Text>
           </View>
-        </SafeAreaView>
+          <View style={styles.calculatorSection}>
+            <View style={styles.currentExpenseSection}>
+              <Text>$00.00</Text>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text>1</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>2</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>3</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>4</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>5</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>6</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>7</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>8</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>9</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>back</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>0</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>check</Text>
+              </TouchableOpacity>
+            </View>
+            <SafeAreaView />
+          </View>
+        </View>
       </Fragment>
     );
   }
@@ -58,39 +98,33 @@ class AddExpenseScreen extends Component {
 export default AddExpenseScreen;
 
 const styles = StyleSheet.create({
-  appScreen: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
   contentWrapper: {
-    flex: 1
+    flex: 1,
+    backgroundColor: COLORS.blueMain,
   },
   categorySection: {
-    backgroundColor: COLORS.blueMain,
     paddingTop: 20,
     paddingHorizontal: 15,
-    paddingBottom: 40
+    paddingBottom: 40,
   },
   calculatorSection: {
     flex: 1,
-    //backgroundColor: COLORS.white,
-    //borderRadius: 20,
-    //paddingTop:20
-    // paddingHorizontal: 15,
-   // paddingVertical: 20,
-    // shadowColor: '#000',
-    // shadowOffset: { width: 0, height: -2 },
-    // shadowOpacity: 0.3,
-    // shadowRadius: 4,
-    //elevation: 1,
-  },
-  currentExpenseSection: {
     backgroundColor: COLORS.white,
     borderRadius: 20,
-    marginTop: -20,
-    minHeight: 40,
-    paddingTop:20,
-    paddingHorizontal: 15
-  }
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 1,
+  },
+  currentExpenseSection: {
+    // backgroundColor: COLORS.white,
+    // borderRadius: 20,
+    // marginTop: -20,
+    // minHeight: 40,
+    // paddingTop: 20,
+    // paddingHorizontal: 15,
+  },
 });
-// TODO FIGURE OUT SHADOW ON TOP ONLY
